6efbb9c0e4c6fa85c93bcd859fa45d331d7c52d8
Alessian Dormal <alessian@alessian.be>
2026-06-08 20:52:24 +0200

    feat: add a pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: discord-source-sanitizer,
+            script: ./dist/main.js,
+            cwd: .,
+            env: {
+                NODE_ENV: production,
+            },
+            exp_backoff_restart_delay: 100,
+            max_restarts: 10,
+            min_uptime: 10s,
+            watch: [dist],
+            kill_timeout: 5000, //allow time for shutdown to happen
+        },
+    ],
+};
