d54cea960f4acd2566de926499e658a4a9270322
Alessian Dormal <alessian@alessian.be>
2026-06-19 17:24:32 +0200

    fix: malformed ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
     apps: [
         {
-            name: discord-source-sanitizer,
-            script: ./dist/main.js,
-            cwd: .,
+            name: "discord-source-sanitizer",
+            script: "./dist/main.js",
+            cwd: ".",
             env: {
-                NODE_ENV: production,
+                NODE_ENV: "production",
             },
             exp_backoff_restart_delay: 100,
             max_restarts: 10,
-            min_uptime: 10s,
-            watch: [dist],
+            min_uptime: "10s",
+            watch: ["dist"],
             kill_timeout: 5000, //allow time for shutdown to happen
         },
     ],
